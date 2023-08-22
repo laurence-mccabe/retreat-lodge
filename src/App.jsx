7e6cb1bfb53f45e2b648a316db83a8ff07a756c2
laurence0202 <laurence0202@users.noreply.github.com>
2023-08-22 15:11:54 +1000

    setup initial files for the-retreat-lodge

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const H1 = styled.h1`
+font-size: 30px;
+font-weight: 600;`
+
+const App = () => {
+  return (
+    <div>
+      <H1>App</H1> 
+    </div>
+  )
+}
+
+export default App
