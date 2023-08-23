33b51483d6f053cf3897505ff120eabd03f9db71
laurence0202 <laurence0202@users.noreply.github.com>
2023-08-23 10:27:18 +1000

    added styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,53 @@
 import styled from 'styled-components'
+import GlobalStyles from './styles/GlobalStyles'
+import Button from './ui/Button'
+import Input from './ui/Input'
+import Heading from './ui/Heading'
+import Row from './ui/Row'
 
-const H1 = styled.h1`
-font-size: 30px;
-font-weight: 600;`
+const StyledApp = styled.main`
+  background-color: #cdcdd7;
+  padding: 20px;
+`
 
 const App = () => {
   return (
-    <div>
-      <H1>App</H1> 
-    </div>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">App</Heading>
+
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button
+                variation="primary"
+                size="medium"
+                onClick={() => alert('check in')}
+              >
+                Check in
+              </Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert('check out')}
+              >
+                Check Out
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
   )
 }
 
