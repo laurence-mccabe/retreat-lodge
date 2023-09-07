9f661f4c05d74c168841b62eab7b5c7e2a1448e4
laurence0202 <laurence0202@users.noreply.github.com>
2023-09-07 15:11:25 +1000

    29.2  08:00 discount tab highlight working

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,17 +2,18 @@ import Heading from '../ui/Heading'
 import Row from '../ui/Row'
 import CabinTable from '../features/cabins/CabinTable'
 import AddCabin from '../features/cabins/AddCabin'
+import CabinTableOperations from '../features/cabins/CabinTableOperations'
 
 function Cabins() {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter / Sort</p>
+        <CabinTableOperations></CabinTableOperations>
       </Row>
       <Row>
         <CabinTable />
-         <AddCabin />
+        <AddCabin />
       </Row>
     </>
   )
