03a7bea99f28a283e85bbcd310d700cf7efe7407
laurence0202 <laurence0202@users.noreply.github.com>
2023-09-07 15:20:27 +1000

    29.2 10:25

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -4,6 +4,7 @@ import CabinRow from './CabinRow'
 import { useCabins } from './useCabins'
 import Table from '../../ui/Table'
 import Menus from '../../ui/Menus'
+import { useSearchParams } from 'react-router-dom'
 
 // const Table = styled.div`
 //   border: 1px solid var(--color-grey-200);
@@ -30,9 +31,18 @@ const TableHeader = styled.header`
 `
 
 const CabinTable = () => {
-  const { isLoading, cabins, error } = useCabins()
+  const { isLoading, cabins } = useCabins()
+  const [searchParams] = useSearchParams()
 
   if (isLoading) return <Spinner />
+
+  const filterValue = searchParams.get('discount') || 'all'
+  console.log(filterValue)
+
+  let filteredCabins;
+  if (filterValue === 'all') filteredCabins = cabins
+  if (filterValue === 'no-discount') filteredCabins = cabins.filter(cabin => cabin.discount === 0)
+  if (filterValue === 'with-discount') filteredCabins = cabins.filter(cabin => cabin.discount > 0)
 
   return (
     <Menus>
