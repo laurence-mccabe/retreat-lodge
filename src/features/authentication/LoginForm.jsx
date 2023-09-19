6da5383640c882d391f2c94186ae7daf9cccb361
laurence0202 <laurence0202@users.noreply.github.com>
2023-09-19 13:14:02 +1000

    added login ability for user with supabase

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -1,14 +1,21 @@
-import { useState } from "react";
-import Button from "../../ui/Button";
-import Form from "../../ui/Form";
-import Input from "../../ui/Input";
-import FormRowVertical from "../../ui/FormRowVertical";
+import { useState } from 'react'
+import Button from '../../ui/Button'
+import Form from '../../ui/Form'
+import Input from '../../ui/Input'
+import FormRowVertical from '../../ui/FormRowVertical'
+import { useLogin } from './useLogin'
+import SpinnerMini from '../../ui/SpinnerMini'
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('laurence@example.com')
+  const [password, setPassword] = useState('')
+  const { login, isLoading } = useLogin()
 
-  function handleSubmit() {}
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (!email || !password) return
+    login({ email, password })
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -20,6 +27,7 @@ function LoginForm() {
           autoComplete="username"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          disabled={isLoading}
         />
       </FormRowVertical>
       <FormRowVertical label="Password">
@@ -29,13 +37,16 @@ function LoginForm() {
           autoComplete="current-password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          disabled={isLoading}
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button size="large">Login</Button>
+        <Button size="large" disabled={isLoading}>
+          {!isLoading ? 'Login' : <SpinnerMini />}
+        </Button>
       </FormRowVertical>
     </Form>
-  );
+  )
 }
 
-export default LoginForm;
+export default LoginForm
---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,22 @@
+import { useMutation } from '@tanstack/react-query'
+import { login as loginAPI } from '../../services/apiAuth'
+import { useNavigate } from 'react-router-dom'
+import toast from 'react-hot-toast'
+
+export function useLogin() {
+  const navigate = useNavigate()
+
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: ({ email, password }) => loginAPI({ email, password }),
+    onSuccess: (user) => {
+        console.log(user)
+      navigate('/dashboard')
+    },
+    onError: (error) => {
+      console.log('ERROR', error)
+      toast.error("Provided email and/or password are incorrect.")
+    },
+  })
+  return {login, isLoading}
+}
+
