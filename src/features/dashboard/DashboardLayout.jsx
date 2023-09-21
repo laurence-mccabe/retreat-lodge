65413bf361481990dd8de6e00b6f79aa53bfd994
laurence0202 <laurence0202@users.noreply.github.com>
2023-09-21 14:15:02 +1000

    added charting sales data from bookings using recharts.org

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import Spinner from '../../ui/Spinner'
 import useRecentStays from './useRecentStays'
 import Stats from './Stats'
 import useCabins from '../cabins/useCabins'
+import SalesChart from './SalesChart'
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -31,7 +32,7 @@ const DashBoardLayout = (props) => {
       ></Stats>
       <div>todays activities</div>
       <div>chart stay durations</div>
-      <div>chart sales</div>
+      <SalesChart bookings ={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   )
 }
