aa2b2178b865617193e9980f64a9c75c0ee07692
laurence0202 <laurence0202@users.noreply.github.com>
2023-10-11 16:04:38 +1100

    updated login creds to default access

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from './useLogin'
 import SpinnerMini from '../../ui/SpinnerMini'
 
 function LoginForm() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('laurence@example.com')
+  const [password, setPassword] = useState('11111111')
   const { login, isLoading } = useLogin()
 
   function handleSubmit(e) {
